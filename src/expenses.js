871a1a1fcadee95882c96c87bcc22afbaca3273f
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2022-10-15 10:35:38 +0300

    add row creation from form

--- src/expenses.js
+++ src/expenses.js
@@ -1,3 +1,6 @@
+import { e, tr, td, months, categories } from "./util";
+
+const tbody = document.querySelector("tbody");
 const form = document.getElementById("new-expense");
 
 form.addEventListener("submit", onSubmit);
@@ -9,5 +12,28 @@ function onSubmit(event) {
 
   const data = Object.fromEntries(formData);
 
+  for (const key in data) {
+    if (data[key] == "") {
+      // add error message
+      return;
+    }
+  }
   console.log(data);
+
+  const row = createRow(data);
+  tbody.appendChild(row);
+}
+
+function createRow({ date, name, category, amount }) {
+  const dateValue = new Date(date);
+
+  const row = tr(
+    td(`${dateValue.getDate()}.${months[dateValue.getMonth()]}`),
+    td(name),
+    td(categories[category]),
+    td(e("span", { className: "currency" }, amount)),
+    td(e("button", {}, "Edit"), e("button", {}, "Delete"))
+  );
+
+  return row;
 }
--- src/util.js
+++ src/util.js
@@ -18,3 +18,26 @@ export function e(type, attributes, ...content) {
 
 export const tr = e.bind(null, "tr", {});
 export const td = e.bind(null, "td", {});
+
+export const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+export const categories = {
+  0: "Other",
+  1: "Utilities",
+  2: "Groceries",
+  3: "Entertainment",
+  4: "Transport",
+};
