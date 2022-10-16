91cb4d5a0d0700b4560230db4be8e0766e675831
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2022-10-16 10:27:34 +0300

    add row creation in budget form

---
+++ src/budget.js
@@ -0,0 +1,44 @@
+import { e, tr, td, months } from "./util";
+
+const budget = new Map();
+
+const tbody = document.querySelector("tbody");
+const form = document.getElementById("new-budget");
+
+form.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+
+  const data = Object.fromEntries(formData);
+
+  // handle if data == "";
+  const row = createRow(data);
+
+  let id = data.month;
+  budget.set(id, data);
+
+  console.log(budget);
+
+  form.reset();
+  tbody.appendChild(row);
+}
+
+function createRow({ month, income, budget }) {
+  let [monthAsStr, yearAsStr] = month.split("-");
+
+  monthAsStr = monthAsStr.startsWith("0")
+    ? Number(monthAsStr.slice(1, 2) - 1)
+    : monthAsStr;
+
+  const row = tr(
+    td(`${months[monthAsStr]}.${yearAsStr}`),
+    td(e("span", { className: "currency" }, income)),
+    td(e("span", { className: "currency" }, budget)),
+    td(e("button", {}, "Edit"), e("button", {}, "Delete"))
+  );
+
+  return row;
+}
