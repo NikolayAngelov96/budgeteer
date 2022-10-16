4862a019520bcd43e484e0b620d7ed592a2b98fe
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2022-10-16 22:43:32 +0300

    display overview data

--- src/dashboard.js
+++ src/dashboard.js
@@ -3,7 +3,51 @@ import { getData, e, categories } from "./util";
 const breakdown = getBreakdownData();
 const breakdownContainer = document.querySelector(".container");
 
+const overviewNumbers = document.querySelector(".left-col");
+
+// display overview bars properly
+
 displayBreakdownData();
+displayOverviewData();
+
+function getOverviewData() {
+  const spent = Object.values(breakdown).reduce((acc, c) => acc + c, 0);
+
+  const budget = getData("budget");
+
+  let budgetValue = 0;
+  let totalIncome = 0;
+
+  for (const item of [...budget.values()]) {
+    budgetValue += Number(item.budget);
+    totalIncome += Number(item.income);
+  }
+
+  let remaining = budgetValue - spent;
+
+  let savings = totalIncome - budgetValue;
+
+  return {
+    Spent: spent,
+    Remaining: remaining,
+    Savings: savings,
+  };
+}
+
+function displayOverviewData() {
+  const overviewData = getOverviewData();
+
+  for (const key in overviewData) {
+    const element = e(
+      "div",
+      { className: "cat-row" },
+      e("span", { className: "row label" }, key),
+      e("span", { className: "row value" }, overviewData[key])
+    );
+
+    overviewNumbers.appendChild(element);
+  }
+}
 
 function getBreakdownData() {
   const records = getData("records");
